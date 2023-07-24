6d8b7831b99be9ce9f90783385fccd1b146f6259
Sahil <garg07825@gmail.com>
2023-07-24 18:04:51 +0530

    added all route

--- controlers/userFun.js
+++ controlers/userFun.js
@@ -4,46 +4,47 @@ import { sendCookie } from "../uils/features.js";
 import ErrorHandler from "../uils/errorHandler.js";
 
 // get all users infromation router
-export const getAllUser = async (req, res) => {};
+export const getAllUser = async (req, res) => {
+  res.send("nice working");
+};
 
 // register router
 export const register = async (req, res, next) => {
- try{
-  const { name, email, password } = req.body;
+  try {
+    const { name, email, password } = req.body;
 
-  let user = await User.findOne({ email });
+    let user = await User.findOne({ email });
 
-  if (user) return next(new ErrorHandler("user already exist", 404));
-   
-  const hashpassword = await bcrypt.hash(password, 10);
+    if (user) return next(new ErrorHandler("user already exist", 404));
 
-  user = await User.create({ name, email, password: hashpassword });
+    const hashpassword = await bcrypt.hash(password, 10);
 
-  // calling a function from another utils
-  sendCookie(res, user, 201, "Registerd Succesfully");
+    user = await User.create({ name, email, password: hashpassword });
 
- }catch(error){
-  next(error);
- }
+    // calling a function from another utils
+    sendCookie(res, user, 201, "Registerd Succesfully");
+  } catch (error) {
+    next(error);
+  }
 };
 
-// handling login router 
+// handling login router
 export const login = async (req, res, next) => {
-try{
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  let user = await User.findOne({ email }).select("+password");
+    let user = await User.findOne({ email }).select("+password");
 
-  if (!user) return next(new ErrorHandler("Invalid email", 400));
+    if (!user) return next(new ErrorHandler("Invalid email", 400));
 
-  const ismatch = await bcrypt.compare(password, user.password);
+    const ismatch = await bcrypt.compare(password, user.password);
 
-  if (!ismatch) return next(new ErrorHandler("Invalid password"));
+    if (!ismatch) return next(new ErrorHandler("Invalid password"));
 
-  sendCookie(res, user, 200, `Welcome back ${user.name}`);
-}catch(error){
-next(error);
-}
+    sendCookie(res, user, 200, `Welcome back ${user.name}`);
+  } catch (error) {
+    next(error);
+  }
 };
 
 // to know the current user session
@@ -60,8 +61,8 @@ export const logout = (req, res) => {
     .status(200)
     .cookie("token", "", {
       expires: new Date(Date.now()),
-      sameSite : process.env.NODE_ENV === "development" ? "lax" : "none",
-      secure : process.env.NODE_ENV === "development" ? false : true
+      sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
+      secure: process.env.NODE_ENV === "development" ? false : true,
     })
     .json({
       success: true,
